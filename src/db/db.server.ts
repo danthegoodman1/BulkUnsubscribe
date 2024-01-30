66a3329e4fe47c2895c9324dd3682e0539f23320
Dan Goodman <xxdanthegoodmanxx@gmail.com>
2024-01-30 16:17:04 -0500

    normal sync

--- src/db/db.server.ts
+++ src/db/db.server.ts
@@ -14,6 +14,7 @@ export const db = await open({
 export async function initDB() {
   await db.exec("PRAGMA journal_mode = WAL;")
   await db.exec("PRAGMA busy_timeout = 5000;")
+  await db.exec("PRAGMA synchronous = NORMAL;")
   logger.debug(`Using db file "${dbFileName}"`)
   const schema = await readFile(path.join("src", "db", "schema.sql"), "utf-8")
   logger.debug(`Running schema.sql`)
